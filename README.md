516ca4bd25fbebe2d17765f42119a464dcebb59c
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2021-08-28 06:44:30 +0900

    used resample function to convert t into uniform interval

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # LFP-matlab
  matlab code for sensor signal analysis
 
-1. 
+1. 300-3000hz nadas filter
--- sensor_analysis.m
+++ sensor_analysis.m
@@ -1,10 +1,10 @@
 %load dataset
 % use 16 digits of precision(MATLAB default)
-format long % display numbers up to 15 digits
 
 data = readmatrix("raw data.csv");
 binTimeStart = data(:,5);
 binTimeInterval = diff(binTimeStart);
+binTimeInterval(end+1) = binTimeInterval(end)
 
 %turn MFP values into a single 1d array
 LFPValues = data(:,6:end);
@@ -13,9 +13,16 @@ LFPValues = LFPValues.'; %transpose
 LFPValues = LFPValues(:); % collapse into 1d array, columnwisely
 
 pointIndexPerBin = 0 : (nPointsPerBin-1);
-time = binTimeStart + binTimeInterval * (pointIndexPerBin/256);
-time = time.';
-time = time(:);
+t = binTimeStart + binTimeInterval * (pointIndexPerBin/256);
+t = t.';
+t = t(:);
+Ts = mean(binTimeInterval)/256
+desiredFs = 1/Ts;
 
+[LFPValuesResampled, tResampled] = resample(LFPValues, t, desiredFs, 'spline')
 
-plot(time, LFPValues)
+plot(t, LFPValues)
+hold on
+plot(tResampled, LFPValuesResampled)
+hold off
+legend('Original', 'Resampled using spline')
