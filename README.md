19f4a311af0b82d38d9d35fec6734a7d25cf7224
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2021-08-28 08:57:40 +0900

    completed spike detection and raster plot

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
  matlab code for sensor signal analysis
 
 MATLAB signal processing toolbox
-
+Statistics and machine learning toolbox
 1. 300-3000hz nadas filter
--- sensor_analysis.m
+++ sensor_analysis.m
@@ -19,8 +19,8 @@ t = t.';
 t = t(:);
 
 %resample the signal using spline, to make uniform time interval
-Ts = mean(binTimeInterval)/256
-desiredFs = 1/Ts;
+Ts = mean(binTimeInterval)/256 % milisecond per timestamp
+desiredFs = 1/Ts %approx. 3000hz. 1/10 of reference code 30000hz
 [LFPValuesResampled, tResampled] = resample(LFPValues, t, desiredFs, 'spline');
 
 plot(t, LFPValues)
@@ -30,11 +30,59 @@ hold off
 legend('Original', 'Resampled using spline')
 
 %apply xxx-xxxx Hz bandpath filter
-LFPValuesfiltered = bandpass(LFPValuesResampled, [500, 1000], desiredFs)
+LFPValuesfiltered = bandpass(LFPValuesResampled, [1000, 2000], desiredFs);
 
-subplot(2,1,1);
+subplot(3,1,1);
 plot(tResampled, LFPValuesResampled)
-subplot(2,1,2);
+xlim([min(tResampled), max(tResampled)])
+
+subplot(3,1,2);
 plot(tResampled, LFPValuesfiltered)
+xlim([min(tResampled), max(tResampled)])
 
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%paramter setting
+thres = 10; %define multiple of sigma
+threshold = -thres.* median(abs(LFPValuesfiltered)/0.6745); %approximate standard deviation with probable error
+waveformHalfWidth = 600 %reference code: 2 * 30 = 60
+time_stampl = []
+waveform = []
+
+%%spike detection
+count = 0;
+ii = waveformHalfWidth
+while ii < length(LFPValuesfiltered)
+    tmp = LFPValuesfiltered(ii);
+    if tmp < threshold
+        if ii + waveformHalfWidth < length(LFPValuesfiltered) % spike가 뒷쪽에 있을 경우 waveformwidth가 확보될 때만 기록한다.
+            count = count + 1;
+            time_stamp(count) = ii;
+            waveform(count,:) = LFPValuesfiltered( (-waveformHalfWidth : waveformHalfWidth) + ii );
+        end
+        ii = ii + waveformHalfWidth;
+    else
+        ii = ii + 1;
+    end
+end
+            
+
+%plot((-waveformHalfWidth : waveformHalfWidth)*Ts, waveform')
+%xlabel('time(ms)')
+%ylabel('Voltage(uV)')
+
+
+
+%raster plot
+subplot(3,1,3);
+hold on
+for ii = 1:length(time_stamp)
+    time_stamp(ii)
+    plot(binTimeStart(1) + [time_stamp(ii), time_stamp(ii)]*Ts, [-1,1], 'k');
+end
+xlim([min(tResampled), max(tResampled)])
+ylim([-10,10])
+
+%%dimension reduction (PCA)
+waveform = zscore(waveform)
