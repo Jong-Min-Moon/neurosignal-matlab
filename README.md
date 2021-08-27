babcefc91dea9715f4a9aacc71084e1fd177cdd0
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2021-08-28 07:52:31 +0900

    completed bandpass filter

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # LFP-matlab
  matlab code for sensor signal analysis
 
+MATLAB signal processing toolbox
+
 1. 300-3000hz nadas filter
--- sensor_analysis.m
+++ sensor_analysis.m
@@ -12,17 +12,29 @@ LFPValues = data(:,6:end);
 LFPValues = LFPValues.'; %transpose
 LFPValues = LFPValues(:); % collapse into 1d array, columnwisely
 
-pointIndexPerBin = 0 : (nPointsPerBin-1);
-t = binTimeStart + binTimeInterval * (pointIndexPerBin/256);
+%time variable into a single 1d array
+tPercentileInsideBin = linspace(0,255/256,256)
+t = binTimeStart + binTimeInterval * tPercentileInsideBin;
 t = t.';
 t = t(:);
+
+%resample the signal using spline, to make uniform time interval
 Ts = mean(binTimeInterval)/256
 desiredFs = 1/Ts;
-
-[LFPValuesResampled, tResampled] = resample(LFPValues, t, desiredFs, 'spline')
+[LFPValuesResampled, tResampled] = resample(LFPValues, t, desiredFs, 'spline');
 
 plot(t, LFPValues)
 hold on
 plot(tResampled, LFPValuesResampled)
 hold off
 legend('Original', 'Resampled using spline')
+
+%apply xxx-xxxx Hz bandpath filter
+LFPValuesfiltered = bandpass(LFPValuesResampled, [500, 1000], desiredFs)
+
+subplot(2,1,1);
+plot(tResampled, LFPValuesResampled)
+subplot(2,1,2);
+plot(tResampled, LFPValuesfiltered)
+
+
