53643bb291d4c6625bfa4b3de8b85a329600ed75
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2022-04-27 18:03:18 +0900

    mean RMS, SNR

--- meanLocalPeakToPeak.m
+++ meanLocalPeakToPeak.m
@@ -1,5 +1,13 @@
-function result = meanLocalPeakToPeak(t, signal)
-    [localMaxValues, ] = findpeaks(signal, t);
-    [localMinValues, ] = findpeaks(-signal, t);
-    result = mean(localMaxValues) + mean(localMinValues);
+function [meanPeakToPeak, localMaxValueSelected, localMaxLocSelected, localMinValueSelected, localMinLocSelected] = meanLocalPeakToPeak(t, signal, threshold)
+    [localMaxValue, localMaxLoc] = findpeaks(signal, t);
+    [localMinValue, localMinLoc] = findpeaks(-signal, t);
+    
+    localMaxValueSelected = localMaxValue(localMaxValue > threshold);
+    localMaxLocSelected   = localMaxLoc(localMaxValue > threshold);
+    localMinValueSelected = localMinValue(localMinValue > threshold);
+    localMinLocSelected   = localMinLoc(localMinValue > threshold);
+    
+    meanPeakToPeak = mean(localMaxValueSelected) + mean(localMinValueSelected);
+
+    localMinValueSelected = -localMinValueSelected;
 end
