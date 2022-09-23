e1b977016dec65cef6ba3cb2831d453034b939bb
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2022-09-23 20:34:52 +0900

    [20:34]3d plot

--- drawnx.py
+++ drawnx.py
@@ -9,6 +9,9 @@ import scipy
 import pickle
 import pandas as pd   
 import os
+import plotly.graph_objects as go
+
+
 pwd = os.getcwd()
 #1.  create network.
 distMat = np.load(pwd + "/score_matrix.npy")
@@ -27,7 +30,8 @@ channel_list = ["{}".format(int(i)) for i in positions[0]]
 for i in range(len(channel_list)):
     x_now = positions.iloc[i, 1]
     y_now = positions.iloc[i, 2]
-    position_now = (int(x_now), int(y_now))
+    z_now = positions.iloc[i, 3]
+    position_now = (int(x_now), int(y_now), int(z_now))
     G.add_node(channel_list[i], pos = position_now )
     #pos[channel_list[i]] = position_now
 pos=nx.get_node_attributes(G,'pos')
@@ -65,8 +69,8 @@ for i in range(len(G.nodes())):
            color_list_community[i] = j
            
 
-# Plot Community           
-fig, ax = plt.subplots()
+
+
 edges = G.edges()
 weights = [G[u][v]['weight'] for u, v in edges]
 Feature_color_sub = color_list_community
@@ -74,33 +78,84 @@ Feature_color_sub = color_list_community
 d = dict(G.degree)
 node_size_normalized = np.fromiter(d.values(), dtype = float)
 node_size_normalized = (node_size_normalized - np.min(node_size_normalized))/(np.max(node_size_normalized) - np.min(node_size_normalized))
-nx.draw_networkx_nodes(
-    G=G,
-    pos = pos,
-    node_size = 150 * (1 + node_size_normalized),
-    node_color=Feature_color_sub,
-    cmap='gist_ncar',
-    vmin=0,
-    vmax=community_num_group,
-    ax = ax 
-    )
 
-nx.draw_networkx_labels(
-    G=G,
-    pos = pos,
-    font_color = "white",
-    ax = ax )
 
-edges = nx.draw_networkx_edges(
-    G = G,
-    pos = pos,
-    edge_color = weights,
-    width = 1.5,
-    edge_cmap = plt.cm.bwr,
-    ax = ax)
-        #    %pyrun("plt.xticks([])")
-        #    %pyrun("plt.yticks([])")
-plt.colorbar(edges)
-        #    %pyrun("plt.show(block=False)")
+#we need to seperate the X,Y,Z coordinates for Plotly
+x_nodes = [ pos[ str(i+1) ][0] for i in range(len(pos)) ]# x-coordinates of nodes
+y_nodes = [ pos[ str(i+1) ][1] for i in range(len(pos)) ]# y-coordinates
+z_nodes = [ pos[ str(i+1) ][2] for i in range(len(pos)) ]# z-coordinates
 
-fig.savefig('networkfig.png')
+#we  need to create lists that contain the starting and ending coordinates of each edge.
+edge_list = G.edges()
+x_edges=[]
+y_edges=[]
+z_edges=[]
+
+#need to fill these with all of the coordiates
+for edge in edge_list:
+    #format: [beginning,ending,None]
+    x_coords = [pos[edge[0]][0],pos[edge[1]][0],None]
+    x_edges += x_coords
+
+    y_coords = [pos[edge[0]][1],pos[edge[1]][1],None]
+    y_edges += y_coords
+
+    z_coords = [pos[edge[0]][2],pos[edge[1]][2],None]
+    z_edges += z_coords
+
+trace_edges = go.Scatter3d(
+    name = "Edges",
+    x=x_edges,
+                        y=y_edges,
+                        z=z_edges,
+                        mode='lines',
+                        line=dict(
+                            color= weights,
+                            colorscale=['blue','red'],
+                            width=4,
+                            colorbar=dict(thickness=20, title= "edges", xanchor = "left")                    
+),
+                        hoverinfo='none')
+
+#create a trace for the nodes
+trace_nodes = go.Scatter3d(
+    name = "Community",
+    x=x_nodes,
+    y=y_nodes,
+    z=z_nodes,
+    mode='markers',
+                        marker=dict(
+                            symbol='circle',
+                                    size= 10 + 20 * (1 + node_size_normalized),
+                                    color=Feature_color_sub, #color the nodes according to their community
+                                    colorscale=['lightgreen','magenta'], #either green or mageneta
+                                    line=dict(color='black', width=0.5),
+                                    colorbar=dict(thickness=20, title= "community", xanchor = "right")
+                                    ),
+                        text=Feature_color_sub,
+                        hoverinfo='text',
+                        showlegend = True
+                        )
+
+axis = dict(showbackground=False,
+            showline=False,
+            zeroline=False,
+            showgrid=False,
+            showticklabels=False,
+            title='')
+#also need to create the layout for our plot
+layout = go.Layout(title="Community structure of the electrodes",
+                width=650,
+                height=625,
+                showlegend=False,
+                scene=dict(xaxis=dict(axis),
+                        yaxis=dict(axis),
+                        zaxis=dict(axis),
+                        ),
+                margin=dict(t=100),
+                hovermode='closest')
+
+data = [trace_edges, trace_nodes]
+fig = go.Figure(data=data, layout=layout)
+
+fig.write_html(pwd + "/networkfig.html")
